6a7faa169a7f9762105548c954be31ea174a1f7c
Mikhail <mishken3@gmail.com>
2022-05-19 14:30:14 +0300

    add selected class for tabs

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -1,20 +1,27 @@
 import React, { FC, ReactNode } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Wrapper.module.css';
+import cn from 'classnames';
 
 interface WrapperProps {
   children: ReactNode;
 }
 
 export const Wrapper: FC<WrapperProps> = ({ children }) => {
+  const isPersonalPage = window.location.pathname === '/';
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.page}>
         <div className={styles.tabs}>
-          <Link className={styles.tabs__item} to="/converter">
+          <Link
+            className={cn(styles.tabs__item, !isPersonalPage && styles.tabs__item_selected)}
+            to="/converter">
             Конвертер
           </Link>
-          <Link className={styles.tabs__item} to="/">
+          <Link
+            className={cn(styles.tabs__item, isPersonalPage && styles.tabs__item_selected)}
+            to="/">
             Личный кабинет
           </Link>
         </div>
