e74d2d1050a483eacc7d23a1e49519ff86d81b4b
Mikhail <mishken3@gmail.com>
2022-05-20 17:00:29 +0300

    moved currencies to new file, setup tabs logic

--- src/pages/Converter/components/CurrencyTabs/CurrencyTabs.tsx
+++ src/pages/Converter/components/CurrencyTabs/CurrencyTabs.tsx
@@ -1,40 +1,45 @@
 import cn from 'classnames';
 import React, { FC } from 'react';
 
+import { currencies } from '../../../../data/currencies';
 import styles from './CurrencyTabs.module.scss';
 
 interface CurrencyTabsProps {
   title: string;
+  selectedCurrency: string;
+  handleOnClick: (value: string) => void;
 }
 
-export const CurrencyTabs: FC<CurrencyTabsProps> = ({ title }) => {
-  const currencies = [
-    {
-      name: 'BTC',
-      isSelected: true,
-    },
-    {
-      name: 'ETH',
-      isSelected: false,
-    },
-    {
-      name: 'USD',
-      isSelected: false,
-    },
-  ];
+export const CurrencyTabs: FC<CurrencyTabsProps> = ({ title, selectedCurrency, handleOnClick }) => {
+  /* * TODO:
+  // selectedCurrency === currencies.name
+  const currenciesName = currencies.map((currency) => currency.name); // ['BTC', 'ETH', 'USD']
+   * 1. Сравнивать значения в state
+   * 2. Обновлять значение isSelected
+   * 3. На основе isSelected из массива currencies делать ререндер
+   * */
 
   return (
     <div className={styles.currencies}>
       <p className={styles.currencies__title}>{title}</p>
 
       <div className={styles.currencies__wrapper}>
-        {currencies.map(({ name, isSelected }) => (
-          <div
-            key={name}
-            className={cn(styles.currencies__item, isSelected && styles.currencies__item_active)}>
-            {name}
-          </div>
-        ))}
+        {currencies.map(({ name, isSelected }) => {
+          const checkIsSelected = name === selectedCurrency;
+
+          return (
+            <button
+              type="button"
+              onClick={() => handleOnClick(name)}
+              key={name}
+              className={cn(
+                styles.currencies__item,
+                checkIsSelected && styles.currencies__item_active,
+              )}>
+              {name}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
