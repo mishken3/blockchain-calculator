9cdb856457bbed48d00f2978d0303b7e9c02168b
Mikhail <mishken3@gmail.com>
2022-05-23 17:24:38 +0300

    add header style

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -1,7 +1,6 @@
-import cn from 'classnames';
 import React, { FC, ReactNode } from 'react';
-import { Link } from 'react-router-dom';
 
+import { Body, Header } from './components';
 import styles from './Wrapper.module.scss';
 
 interface WrapperProps {
@@ -9,26 +8,10 @@ interface WrapperProps {
 }
 
 export const Wrapper: FC<WrapperProps> = ({ children }) => {
-  const isPersonalPage = window.location.pathname === '/';
-
   return (
     <div className={styles.wrapper}>
-      <div className={styles.page}>
-        <div className={styles.tabs}>
-          <Link
-            className={cn(styles.tabs__item, !isPersonalPage && styles.tabs__item_selected)}
-            to="/converter">
-            Конвертер
-          </Link>
-          <Link
-            className={cn(styles.tabs__item, isPersonalPage && styles.tabs__item_selected)}
-            to="/">
-            Личный кабинет
-          </Link>
-        </div>
-
-        <div className={styles.content}>{children}</div>
-      </div>
+      <Header />
+      <Body>{children}</Body>
     </div>
   );
 };
