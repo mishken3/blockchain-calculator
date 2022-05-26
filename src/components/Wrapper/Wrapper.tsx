0c26660647143db12726063691b550e78b70b3de
Mikhail <mishken3@gmail.com>
2022-05-26 10:30:47 +0300

    removed Body component

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import { Body, Header } from './components';
+import { Header } from './components';
 import styles from './Wrapper.module.scss';
 
 interface WrapperProps {
@@ -14,9 +14,9 @@ export const Wrapper: FC<WrapperProps> = ({ pageName }) => {
       <Header />
       <div className={styles.page}>
         <p className={styles.page__name}>"{pageName}" pageName doesnt work</p>
-        <Body>
+        <div className={styles.content}>
           <Outlet />
-        </Body>
+        </div>
       </div>
     </div>
   );
