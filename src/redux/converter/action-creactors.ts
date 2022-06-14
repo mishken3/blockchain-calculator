257ec27c7b5eaa5575ec0be64c77caf35f0e2a81
Mikhail <mishken3@gmail.com>
2022-06-14 21:31:20 +0300

    add converter: actions, types

---
+++ src/redux/converter/action-creactors.ts
@@ -0,0 +1,44 @@
+import { CurrenciesEnum } from '../../pages/Converter/components';
+import { CONVERTER_ACTION_TYPES } from './action-types';
+import {
+  ChangeInputAction,
+  ChangeInputExchangeAction,
+  ChangeOutputAction,
+  ChangeOutputExchangeAction,
+  ReverseTabsAction,
+  SelectInputTabAction,
+  SelectOutputTabAction,
+} from './types';
+
+export const selectInputTab = (tabName: CurrenciesEnum): SelectInputTabAction => ({
+  type: CONVERTER_ACTION_TYPES.SELECT_INPUT_TAB,
+  payload: tabName,
+});
+
+export const selectOutputTab = (tabName: CurrenciesEnum): SelectOutputTabAction => ({
+  type: CONVERTER_ACTION_TYPES.SELECT_OUTPUT_TAB,
+  payload: tabName,
+});
+
+export const reverseTabs = (): ReverseTabsAction => ({
+  type: CONVERTER_ACTION_TYPES.REVERSE_TABS,
+});
+
+export const changeInput = (inputValue: number): ChangeInputAction => ({
+  type: CONVERTER_ACTION_TYPES.CHANGE_INPUT,
+  payload: inputValue,
+});
+
+export const changeOutput = (): ChangeOutputAction => ({
+  type: CONVERTER_ACTION_TYPES.CHANGE_OUTPUT,
+});
+
+export const changeInputExchange = (course: number): ChangeInputExchangeAction => ({
+  type: CONVERTER_ACTION_TYPES.CHANGE_INPUT_EXCHANGE_COURSE,
+  payload: course,
+});
+
+export const changeOutputExchange = (course: number): ChangeOutputExchangeAction => ({
+  type: CONVERTER_ACTION_TYPES.CHANGE_OUTPUT_EXCHANGE_COURSE,
+  payload: course,
+});
