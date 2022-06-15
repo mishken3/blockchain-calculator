c3091474ef37850ee978a9aa135ea684d35b61fa
Mikhail <mishken3@gmail.com>
2022-06-15 15:24:14 +0300

    add coins icons

--- src/pages/Personal/components/CurrencyAmount/CurrencyAmount.tsx
+++ src/pages/Personal/components/CurrencyAmount/CurrencyAmount.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 
+import { Coins } from '../../../../assets/icons';
 import { CurrenciesEnum } from '../../../Converter/components';
 import styles from './CurrencyAmount.module.scss';
 
@@ -11,7 +12,7 @@ interface CurrencyAmountProps {
 export const CurrencyAmount: FC<CurrencyAmountProps> = ({ coinName, coinAmount }) => {
   return (
     <div className={styles.currency}>
-      <img className={styles.currency__logo} alt={coinName} />
+      <img className={styles.currency__logo} src={Coins[coinName]} alt={coinName} />
 
       <div className={styles.currency__money}>
         <p className={styles.currency__money_total}>Total</p>
