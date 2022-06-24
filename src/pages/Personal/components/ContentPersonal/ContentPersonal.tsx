e04f415dca991718d90ea635244ade2cddb05e6f
Mikhail <mishken3@gmail.com>
2022-06-24 13:21:33 +0300

    ContentPersonal comp styled and prepared for input

--- src/pages/Personal/components/ContentPersonal/ContentPersonal.tsx
+++ src/pages/Personal/components/ContentPersonal/ContentPersonal.tsx
@@ -10,26 +10,28 @@ interface ContentPersonalProps {
 
 export const ContentPersonal: FC<ContentPersonalProps> = ({ walletAmount, coinItems }) => {
   return (
-    <>
-      <div className={styles.layout}>
-        <div className={styles.heading}>
-          <h3 className={styles.heading__title}>Баланс аккаунта</h3>
-          <p className={styles.heading__balance}>
-            {walletAmount} <span className={styles.heading__balance_currency}>USD</span>
-          </p>
+    <div className={styles.layout}>
+      <div className={styles.heading}>
+        <h3 className={styles.heading__title}>Баланс аккаунта</h3>
+        <p className={styles.heading__balance}>
+          {walletAmount} <span>USD</span>
+        </p>
+      </div>
+
+      <div className={styles.content}>
+        <Diagramm walletAmount={walletAmount} />
+
+        <div className={styles.content__coins}>
+          {coinItems}
+          <button className={styles.content__coins_button}>
+            <span>Exchange</span>
+          </button>
         </div>
 
-        <div className={styles.amount}>
-          <Diagramm walletAmount={walletAmount} />
-
-          <div className={styles.amount__coins}>
-            {coinItems}
-            <button>
-              <span>Exchange</span>
-            </button>
-          </div>
+        <div className={styles.content__exchange}>
+          <input />
         </div>
       </div>
-    </>
+    </div>
   );
 };
