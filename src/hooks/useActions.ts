3a1df2ce79865f8c349921e52c346bfd14a7bdfb
Mikhail <mishken3@gmail.com>
2022-06-28 12:14:26 +0300

    renamed converterActions -> inputActions

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -1,14 +1,14 @@
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import * as converterActions from '../redux/reducers/input/action-creactors';
+import * as inputActions from '../redux/reducers/input/action-creactors';
 import * as walletActions from './../redux/reducers/wallet/action-creactors';
 
 export const useActions = () => {
   const dispatch = useDispatch();
 
   const allActions = {
-    ...converterActions,
+    ...inputActions,
     ...walletActions,
   };
 
